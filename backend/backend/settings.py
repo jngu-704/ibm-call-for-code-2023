73aa39953e7f1c5e4f6f2ada8d4216144d1f66ce
jngu-704 <80801878+jngu-704@users.noreply.github.com>
2023-03-16 16:41:32 +1100

    backend added pagination to api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,6 +127,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+}
+
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
