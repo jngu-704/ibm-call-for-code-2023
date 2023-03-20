b6c89f406bd4be0ab66600a1fa1a5c0f241d4209
jngu-704 <80801878+jngu-704@users.noreply.github.com>
2023-03-20 19:31:53 +1100

    frontend added filtering by country

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
-
+import { useState, useRef } from "react";
 import WorldMap from "./components/WorldMap";
 
 function App() {
+  const [location, setLocation] = useState("Australia");
+  const [coordinates, setCoordinates] = useState([-27, 133]);
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    let input = inputRef.current.value;
+
+    setLocation(input.replace(/ /g, "+"));
+  };
   return (
-    <div>
-      <WorldMap />
+    <div class="row">
+      <div class="column">
+        <input ref={inputRef} type="text" id="location" name="location" />
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <div class="column">
+        <WorldMap location={location} coordinates={coordinates} />
+      </div>
     </div>
   );
 }
--- frontend/src/components/WorldMap.js
+++ frontend/src/components/WorldMap.js
@@ -5,23 +5,15 @@ import {
   LayersControl,
   LayerGroup,
 } from "react-leaflet";
-import { useState } from "react";
 import "./WorldMap.css";
 import "leaflet/dist/leaflet.css";
-
 import PowerPlantMarkers from "./PowerPlantMarkers";
 import CityMarkers from "./CityMarkers";
 
-export default function WorldMap() {
-  const [location, setLocation] = useState("Australia");
-
+export default function WorldMap(props) {
   return (
     <>
-      <MapContainer
-        center={[-26.439, 133.2813]}
-        zoom={5}
-        scrollWheelZoom={true}
-      >
+      <MapContainer center={props.coordinates} zoom={5} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -29,12 +21,12 @@ export default function WorldMap() {
         <LayersControl position="topright">
           <LayersControl.Overlay name="Power Plants">
             <LayerGroup>
-              <PowerPlantMarkers country={location} />
+              <PowerPlantMarkers country={props.location} />
             </LayerGroup>
           </LayersControl.Overlay>
           <LayersControl.Overlay name="Cities">
             <LayerGroup>
-              <CityMarkers country={location} />
+              <CityMarkers country={props.location} />
             </LayerGroup>
           </LayersControl.Overlay>
         </LayersControl>
